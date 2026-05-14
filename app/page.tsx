880c77dcde9c9eafa6c034b7632f2bba74196e3e
Jeffrey Uvero <uverojeffreydev@gmail.com>
2026-05-14 17:01:39 +0800

    Add logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className = "dark">
-      <Header />
-    </div>
-  );
+    <Header />
+  )
 }
+
--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,10 +1,19 @@
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
-
+import Image from "next/image"
 export default function Header(){
     return(
         <div className = "border border-[#272A31] flex justify-between py-[21px] px-[24px]">
-            <p className = "flex-1">Cartlab</p>
+            <p className = "flex-1">
+                <Image
+                    className="dark"
+                    src="/logo.png"
+                    alt="Cart Lab logo"
+                    width={100}
+                    height={20}
+                    priority
+                />
+            </p>
             <div className = "flex-2 max-w-[576px]">
                 <div className="relative">
                     <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
