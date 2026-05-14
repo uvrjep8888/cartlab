70e046e6298f2fbd10fcc8c03a59dc7382fa352f
Jeffrey Uvero <uverojeffreydev@gmail.com>
2026-05-14 16:53:31 +0800

    Implement search input ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className = "border border-[#272A31] flex justify-between py-[21px] px-[24px]">
-      <p className = "flex-1">Cartlab</p>
-      <div className = "flex-2">Search Product</div>
-      <div className = "flex-1 flex justify-between">
-          <div> cart icon </div>
-          <p>$159.7</p>
-          <p>User Icon</p>
-      </div>
+    <div className = "dark">
+      <Header />
     </div>
   );
 }
--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,7 +1,21 @@
-
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
 
 export default function Header(){
     return(
-        <div>This is Header</div>
+        <div className = "border border-[#272A31] flex justify-between py-[21px] px-[24px]">
+            <p className = "flex-1">Cartlab</p>
+            <div className = "flex-2 max-w-[576px]">
+                <div className="relative">
+                    <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+                    <Input type="search" placeholder="Search Product" className="pl-8" />
+                </div>
+            </div>
+            <div className = "flex-1 flex justify-end w-[165px]">
+                <div>cart icon </div>
+                <p>$159.7</p>
+                <p>User Icon</p>
+            </div>
+        </div>
     )
 }
