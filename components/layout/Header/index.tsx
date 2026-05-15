d7f38bda1c4af49bef74f7b4cfaf4a9fe0d9f629
Jeffrey Uvero <uverojeffreydev@gmail.com>
2026-05-15 10:52:47 +0800

    Add badges and fix responsiveness

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@/components/ui/input"
+import { Badge, BadgeCheck } from "@/components/ui/badge"
 import { Search } from "lucide-react"
 import Image from "next/image"
 export default function Header(){
@@ -20,10 +21,64 @@ export default function Header(){
                     <Input type="search" placeholder="Search Product" className="pl-8" />
                 </div>
             </div>
-            <div className = "flex-1 flex justify-end w-[165px]">
-                <div>cart icon </div>
-                <p>$159.7</p>
-                <p>User Icon</p>
+            <div className = "flex-1 flex justify-end w-[165px] items-center">
+                
+                <div className="relative inline-block mr-[24px] hidden md:block">
+                    <div className = "mr-[8px]">
+                        <button>
+                            <Image
+                                className="dark"
+                                src="/iconify-icon.png"
+                                alt=""
+                                width={24}
+                                height={24}
+                                priority
+                            />
+                        </button>
+                    </div>
+
+                    {/* Notification Badge */}
+                    <Badge
+                        className="
+                        absolute 
+                        -right-2 
+                        -top-2 
+                        h-[24px]
+                        w-[24px] 
+                        rounded-full 
+                        bg-lime-400 
+                        text-[16px] 
+                        font-black 
+                        text-black
+                        flex 
+                        items-center 
+                        justify-center
+                        border-[3px]
+                        border-black
+                        shadow-md
+                        "
+                    >
+                        3
+                    </Badge>
+                </div>
+                {/* Price Box */}
+                <div className="border-[3px] border-black bg-[#111827] px-4 py-2 shadow-md  mr-[24px] hidden md:block">
+                    <span className="text-[16px] font-extrabold tracking-tight text-white">
+                    $159.97
+                    </span>
+                </div>
+                <div className = "mr-[8px]">
+                    <button>
+                        <Image
+                            className="dark"
+                            src="/user-icon.png"
+                            alt=""
+                            width={40}
+                            height={40}
+                            priority
+                        />
+                    </button>
+                </div>
             </div>
         </div>
     )
